42c55c95bf43c3c5280f7b35a0d2d382000ab56d
Gang Yang <yang57@live.marshall.edu>
2017-10-28 18:10:21 -0400

    implement add/delete/list/get

--- ck-project/ck-project/Controllers/sampleController.cs
+++ ck-project/ck-project/Controllers/sampleController.cs
@@ -14,7 +14,7 @@ namespace ck_project.Controllers
         ckdatabase db = new ckdatabase();
         public ActionResult Index()
         {
-            List<employee> employee_list = db.employees.Where(d => d.users_types.user_type_number==1).ToList();
+            List<employee> employee_list = db.employees.ToList();
             ViewBag.employeelist = employee_list;
             return View();
         }
@@ -25,6 +25,8 @@ namespace ck_project.Controllers
 
             return View("Index");
         }
+
+        //get user info in a editable view
         public ActionResult Mofifyemployee(int id) {
             employee target = db.employees.Where(a => a.emp_number == id).FirstOrDefault();
             var utype = new List<SelectListItem> {
@@ -51,5 +53,91 @@ namespace ck_project.Controllers
             ViewBag.branch = branchtypes;
             return View(target);
         }
+
+        [HttpPost]
+        public ActionResult Viewemployee(int id=2) {
+            employee target = db.employees.Where(e => e.emp_number == id).FirstOrDefault();
+            Console.WriteLine(Request.Form["emp_firstname"]);
+            return View(target);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Add(FormCollection form) {
+            //setting dropdown list for forgern key
+            var utype = new List<SelectListItem> {
+                new SelectListItem{ Text="---select---",Selected=true,Value=""}
+            };
+            utype.AddRange(db.users_types.Select(a => new SelectListItem
+            {
+                Text = a.user_type_name,
+                Selected = false,
+                Value = a.user_type_number.ToString()
+            }));
+
+            var branchtypes = new List<SelectListItem> {
+                new SelectListItem{ Text="---select---",Selected=true,Value=""}
+            };
+            branchtypes.AddRange(db.branches.Select(b => new SelectListItem
+            {
+                Text = b.branch_name,
+                Selected = false,
+                Value = b.branch_number.ToString()
+            }));
+            //setting variable passing
+            ViewBag.utype = utype;
+            ViewBag.branch = branchtypes;
+            //create instance
+            employee target = new employee();
+            //get property
+            TryUpdateModel(target, new string[] { "emp_firstname", "emp_middlename", "emp_lastname", "emp_username", "user_type_number", "branch_number","emp_number","phone_number" }, form.ToValueProvider());
+            //validate
+            if (string.IsNullOrEmpty(target.emp_firstname))
+                ModelState.AddModelError("firstname", "firstname is required");
+
+            if (ModelState.IsValid) {
+                db.employees.Add(target);
+                db.SaveChanges();
+            }
+
+            return View(target);
+        }
+
+        public ActionResult Add() {
+            var utype = new List<SelectListItem> {
+                new SelectListItem{ Text="---select---",Selected=true,Value=""}
+            };
+            utype.AddRange(db.users_types.Select(a => new SelectListItem
+            {
+                Text = a.user_type_name,
+                Selected = false,
+                Value = a.user_type_number.ToString()
+            }));
+
+            var branchtypes = new List<SelectListItem> {
+                new SelectListItem{ Text="---select---",Selected=true,Value=""}
+            };
+            branchtypes.AddRange(db.branches.Select(b => new SelectListItem
+            {
+                Text = b.branch_name,
+                Selected = false,
+                Value = b.branch_number.ToString()
+            }));
+
+            ViewBag.utype = utype;
+            ViewBag.branch = branchtypes;
+            return View();
+        }
+
+        public ActionResult Delete(int id) {
+            //find target by uid
+            employee target = db.employees.First(s => s.emp_number == id);
+            //delete
+            db.employees.Remove(target);
+            db.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
     }
+
 }
