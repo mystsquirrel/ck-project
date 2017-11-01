ed0ea6fa530753017144d24917742f1bc947e8cc
yang <yang57@live.marshall.edu>
2017-11-01 14:04:03 -0400

    merge turkiq till week1

---
+++ ck-project/ck-project/Controllers/TurkiCustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace ck_project.Controllers
+{
+    public class TurkiCustomersController : Controller
+    {
+        // GET: TurkiCustomers
+        //first create the db connecter
+        ckdatabase db = new ckdatabase();
+        public ActionResult Customers()
+        {
+            List<customer> Customers_list = db.customers.ToList();
+            ViewBag.Customerslist = Customers_list;
+            return View();
+        }
+
+
+        public ActionResult CustomersTest()
+        {
+            List<customer> Customers_list = db.customers.ToList();
+            ViewBag.Customerslist = Customers_list;
+            return View();
+        }
+
+    }
+}
