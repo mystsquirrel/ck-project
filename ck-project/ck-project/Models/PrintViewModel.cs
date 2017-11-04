035a83da7c4303ccb55dbfc6348bea7c18b6b807
my <nguyen11@marshall.edu>
2017-11-03 21:36:21 -0400

    update

---
+++ ck-project/ck-project/Models/PrintViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ck_project.Models
+{
+    public class ProposalViewModel
+    {
+        public lead Lead { get; set; }
+        public List<branch> Branch { get; set; }
+        public double AmtDueAtSignProposal { get; set; }
+        public double AmtDueUponStartWork { get; set; }
+        public double AmtDueUponCompletion { get; set; }
+        public total_cost TotalCost { get; set; }
+
+    }
+}
