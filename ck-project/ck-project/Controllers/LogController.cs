f1428f8200604105f5cf4d0776ad1ad3cea766ed
my <nguyen11@marshall.edu>
2017-12-14 07:41:03 -0500

    fix log salesperson search

--- ck-project/ck-project/Controllers/LogController.cs
+++ ck-project/ck-project/Controllers/LogController.cs
@@ -17,28 +17,12 @@ namespace ck_project.Controllers
 
 
 
-        public ActionResult ListLog(int? page, FormCollection fo, string search = null, string Emp = null, string Start = null, string end = null)
+        public ActionResult ListLog(int? page, string search, string Emp, string Start, string end)
         {
-
-            //target.email = form["Item1.email"];
-            //int a = Int32.Parse(form["class_number"]);
-
-            //var Emp1 = fo["item.emp_username"];
-            //var Emp2 = fo["emp_username"];
-            //string Emp3 = fo["item.emp_username"];
-            //string Emp4 = fo["emp_username"];
-            //var Emp5 = fo["Emp"];
-            //string Emp7 = fo["Emp"];
-        
-
-            
-            //      int a = Int32.Parse(fo["item.emp_username"]);
-            //int a2 = Int32.Parse(fo["emp_username"]);
-
-            DateTime start = string.IsNullOrEmpty(Start) ? DateTime.MinValue : DateTime.Parse(Start);
-            DateTime end2 = string.IsNullOrEmpty(end) ? DateTime.MaxValue : DateTime.Parse(end);
+            DateTime startDt = string.IsNullOrEmpty(Start) ? DateTime.MinValue : DateTime.Parse(Start);
+            DateTime endDt = string.IsNullOrEmpty(end) ? DateTime.MaxValue : DateTime.Parse(end);
             TimeSpan ts = new TimeSpan(23, 59, 59);
-            end2 = end2.Date + ts;
+            endDt = endDt.Date + ts;
 
 
             try
@@ -46,7 +30,6 @@ namespace ck_project.Controllers
 
                 var EmpInfo = new List<SelectListItem>();
                 EmpInfo.AddRange(db.employees.Select(b => new SelectListItem
-
                 {
                     Text = b.emp_username,
                     Selected = false,
@@ -54,9 +37,18 @@ namespace ck_project.Controllers
                 }));
                 ViewBag.EmpInfo = EmpInfo;
 
-                var result = db.lead_log_file.Where(l => l.update_date >= start && l.update_date <= end2 &&
-                (string.IsNullOrEmpty(search) || l.lead.project_name.Contains(search))
-              && (string.IsNullOrEmpty(Emp) || l.emp_username ==Emp)).ToList();
+                string employeeUserName = null;
+                if (!string.IsNullOrEmpty(Emp))
+                {
+                    int employeeNbr = Int32.Parse(Emp);
+                    var employee = db.employees.Where(e => e.emp_number == employeeNbr).First();
+                    employeeUserName = employee.emp_username;
+                }
+
+
+                var result = db.lead_log_file.Where(l => l.update_date >= startDt && l.update_date <= endDt
+                                                    && (string.IsNullOrEmpty(search) || l.lead.project_name.Contains(search))
+                                                    && (string.IsNullOrEmpty(employeeUserName) || l.emp_username == employeeUserName)).ToList();
 
 
                 return View(result.ToPagedList(page ?? 1, 8));
